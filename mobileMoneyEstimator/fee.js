f61348f59d4ddb7902a7eb6b228cb943c116ce38
Demmy <demestrinea@gmail.com>
2025-06-21 04:49:49 +0300

    Update fee.js

--- mobileMoneyEstimator/fee.js
+++ mobileMoneyEstimator/fee.js
@@ -34,41 +34,4 @@ console.log("\nSend Money Securely!");}
 
 
 // Call the function
-=======
-//Estimates transaction fees for mobile money transfers
-
-function estimateTransactionFee(){
-
-// Constants for fee calculation
-const FeePercentage = 0.015; // 1.5%
-const MinFee = 10;const Maxfee = 70;
-
-//Prompt user for amount
-const amountToSend = parseFloat(prompt("Unatuma Ngapi? (KES):"));  
-
-// Validate input
-if (isNaN(amountToSend) || amountToSend <= 0){
-console.log("Please enter a valid amount in KES.");
-return;} 
-
-// Calculate fee
-let calculatedFee = amountToSend * FeePercentage;  
-
-// Apply minimum and maximum limits
-if (calculatedFee < MinFee){
-calculatedFee = MinFee;} else if (calculatedFee > MaxFee){
-calculatedFee = MaxFee;}  
-
-// Calculate total amount
-const totalAmount = amountToSend + calculatedFee;
-    
-// Display results
-console.log(`Sending KES ${amountToSend}:`);
-console.log(`Calculated Transaction Fee: KES ${calculatedFee}`);
-console.log(`Total amount to be debited: KES ${totalAmount}`);
-console.log("\nSend Money Securely!");}
-
-
-// Call the function
->>>>>>> 1003fbff6200a72bdf3c7271e5ba55c8acbc7a3c
 estimateTransactionFee();
